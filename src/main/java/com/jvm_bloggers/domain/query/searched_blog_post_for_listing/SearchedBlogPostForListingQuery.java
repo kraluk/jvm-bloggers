e4ee5a13bcec2d04e64ac9420c2036a36005f740
Łukasz Krauzowicz <kraluk@gmail.com>
2026-04-18 22:28:28 +0200

    Remove unused Hibernate Search-related features and replaced them by simple JPA queries

--- src/main/java/com/jvm_bloggers/domain/query/searched_blog_post_for_listing/SearchedBlogPostForListingQuery.java
+++ src/main/java/com/jvm_bloggers/domain/query/searched_blog_post_for_listing/SearchedBlogPostForListingQuery.java
@@ -2,9 +2,13 @@ package com.jvm_bloggers.domain.query.searched_blog_post_for_listing;
 
 import com.jvm_bloggers.entities.blog_post.BlogPost;
 import com.jvm_bloggers.entities.blog_post.BlogPostRepository;
+
 import io.vavr.collection.Array;
 import io.vavr.collection.Traversable;
+
 import lombok.AllArgsConstructor;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,26 +17,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class SearchedBlogPostForListingQuery {
 
-  private final BlogPostRepository blogPostRepository;
+    private final BlogPostRepository blogPostRepository;
 
-  public Traversable<SearchedBlogPostForListing> findByTitleOrTag(String searchPhrase, int page, int pageSize) {
-    return blogPostRepository.findApprovedPostsByTagOrTitle(searchPhrase, page, pageSize).stream()
-        .map(this::fromBlogPost)
-        .collect(Array.collector());
-  }
+    public Traversable<SearchedBlogPostForListing> findByTitleOrTag(String searchPhrase, int page,
+                                                                    int pageSize) {
+        return blogPostRepository.findApprovedPostsByTagOrTitle(searchPhrase,
+                PageRequest.of(page, pageSize)).stream()
+            .map(this::fromBlogPost)
+            .collect(Array.collector());
+    }
 
-  public long countByTitleOrTag(String searchPhrase) {
-    return blogPostRepository.countApprovedPostsByTagOrTitle(searchPhrase);
-  }
+    public long countByTitleOrTag(String searchPhrase) {
+        return blogPostRepository.countApprovedPostsByTagOrTitle(searchPhrase);
+    }
 
-  private SearchedBlogPostForListing fromBlogPost(BlogPost blogPost) {
-    return SearchedBlogPostForListing.builder()
-        .title(blogPost.getTitle())
-        .url(blogPost.getUrl())
-        .publishedTime(blogPost.getPublishedDate())
-        .author(blogPost.getBlog().getAuthor())
-        .authorTwitterHandle(blogPost.getBlog().getTwitter())
-        .build();
-  }
+    private SearchedBlogPostForListing fromBlogPost(BlogPost blogPost) {
+        return SearchedBlogPostForListing.builder()
+            .title(blogPost.getTitle())
+            .url(blogPost.getUrl())
+            .publishedTime(blogPost.getPublishedDate())
+            .author(blogPost.getBlog().getAuthor())
+            .authorTwitterHandle(blogPost.getBlog().getTwitter())
+            .build();
+    }
 
 }
